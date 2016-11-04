c7ce210ec87105cffaa44116aeb1aff10c864fe4
hsanchez <huascar.sanchez@gmail.com>
2016-11-04 08:29:25 -0700

    implements representativeness ranking (plus test)

--- src/test/java/com/vesperin/text/utils/StringsTest.java
+++ src/test/java/com/vesperin/text/utils/StringsTest.java
@@ -26,10 +26,13 @@ public class StringsTest {
     StopWords stopWords = StopWords.CUSTOM;
     stopWords.add("foo");
     final List<String> sorted   = Strings.typicalityRank(unsorted.size(), unsorted);
+    final List<String> repr     = Strings.representativenessRank(unsorted);
 
     assertEquals("Join", sorted.get(0));
     assertEquals("Joint", sorted.get(1));
     assertEquals("Jo", sorted.get(2));
+
+    assertEquals("Jo", repr.get(0));
   }
 
   @Test public void testGroupsPruning() throws Exception {
